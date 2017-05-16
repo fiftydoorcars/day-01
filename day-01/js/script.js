4918605df712a0b270430beea6abb310bd573d50
Zachery Coonce <Zachery Coonce>
2017-05-15 20:57:43 -0700

    Day 01 Lesson

---
+++ day-01/js/script.js
@@ -0,0 +1,50 @@
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+// Meh
+var SuperCamelCase;
+
+//Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log("Sending a Message to the Console");
+
+console.info("Sending an Info Message");
+
+console.warn("Here's Johnny!");
+
+console.error("Woop Woop You Screwed Up!");
+
+//Single Line Comment
+
+/*
+  Multi
+  Line
+  Comment
+  */
+
+var
+
+
+
+                  freedom
+
+
+=
+
+
+
+
+
+"Fack You I ain't Going Back!!!";
+console.log(freedom);
+
+// template string
+
+var javascriptTemplateString = ``
